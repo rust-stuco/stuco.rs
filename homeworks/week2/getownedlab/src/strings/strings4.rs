f0396bfa49f414e80bc2cb56b7129bd5df1037a0
TenType <55125103+TenType@users.noreply.github.com>
2026-01-20 09:50:22 -0500

    style: format getownedlab

--- homeworks/week2/getownedlab/src/strings/strings4.rs
+++ homeworks/week2/getownedlab/src/strings/strings4.rs
@@ -39,7 +39,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
