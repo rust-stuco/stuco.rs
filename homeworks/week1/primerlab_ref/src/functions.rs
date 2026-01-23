dec29e193f3badfec883c83616018c01beb7071f
TenType <55125103+TenType@users.noreply.github.com>
2026-01-22 23:54:19 -0500

    chore: fix clippy errors in primerlab refsol

--- homeworks/week1/primerlab_ref/src/functions.rs
+++ homeworks/week1/primerlab_ref/src/functions.rs
@@ -34,12 +34,12 @@ pub fn is_prime(n: usize) -> bool {
     if n == 2 {
         return true;
     }
-    if n % 2 == 0 {
+    if n.is_multiple_of(2) {
         return false;
     }
-    let max_factor = f64::sqrt(n as f64) as usize + 1;
-    for i in (3..max_factor).step_by(2) {
-        if n % i == 0 {
+    let max_factor = n.isqrt();
+    for i in (3..=max_factor).step_by(2) {
+        if n.is_multiple_of(i) {
             return false;
         }
     }
