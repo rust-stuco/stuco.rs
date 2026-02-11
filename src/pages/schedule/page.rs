9917ee6595036b9f77f64ef5075d113d4aa942d7
Anish Pallati <i@anish.land>
2026-02-11 05:01:00 -0500

    chore: increment week number

--- src/pages/schedule/page.rs
+++ src/pages/schedule/page.rs
@@ -6,7 +6,7 @@ use std::sync::LazyLock;
 const LAST_WEEK_SHOWN: usize = if cfg!(debug_assertions) {
     usize::MAX
 } else {
-    4
+    5
 };
 
 static WEEKS: LazyLock<Vec<Week>> = LazyLock::new(load_weeks);
